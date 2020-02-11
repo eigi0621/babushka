6329462e6e70b09c089ed35972c6d81da0346aaa
eigi0088 <eigi0088@gmail.com>
2020-02-11 12:44:14 +0100

    done without sorting

--- js.js
+++ js.js
@@ -5,16 +5,16 @@ window.onresize = function (event) {
   if (window.matchMedia("(min-width: 1000px)").matches) {
     document.querySelector("#main_container").style.margin = "0 " + (windowWidth - 1000) / 2 + "px";
   } else {
-    document.querySelector("#main_container").style.margin = "0 20px";
+    document.querySelector("#main_container").style.margin = "0";
   }
 }
 
-window.onscroll = function (e) {
+/*window.onscroll = function (e) {
   var speedOfImg = .2;
   var imgSpeed = window.pageYOffset * speedOfImg;
   document.querySelector("#splash").style.transform = "translateY(" + imgSpeed + "px)";
   document.querySelector("#space_top").style.transform = "translateY(" + imgSpeed + "px)";
-}
+}*/
 
 
 
@@ -83,26 +83,28 @@ function visPersoner() {
 
 
 
-
+document.querySelectorAll(".ret3").forEach(ret => {
+  ret.classList.add("pull_down");
+});
 
 function lightbox() {
   document.querySelector("#lightbox_bg").style.display = "inherit";
   document.querySelector("#lightbox_bg").classList.remove("hide_lightbox");
   document.querySelector("#lightbox_bg").classList.add("show_lightbox");
   let thisID = this.querySelector(":nth-child(4)").textContent;
-  document.querySelector(`.ret${thisID}`).classList.add("pull_up");
-  document.querySelector(`.ret${thisID}`).classList.remove("pull_down");
   document.querySelector(`.ret${thisID}`).style.display = "flex";
+  document.querySelector(`.ret${thisID}`).classList.remove("pull_down");
+  document.querySelector(`.ret${thisID}`).classList.add("pull_up");
   document.querySelector("#data_fill2").style.visibility = "visible";
   document.querySelector("#data_fill2").addEventListener("click", hideLightbox);
 }
 
 function hideLightbox() {
-  this.style.visibility = "hidden";
   document.querySelector("#lightbox_bg").classList.remove("show_lightbox");
   document.querySelector("#lightbox_bg").classList.add("hide_lightbox");
   setTimeout(function () {
     document.querySelector("#lightbox_bg").style.display = "none";
+    document.querySelector("#data_fill2").style.visibility = "hidden";
   }, 400);
   document.querySelectorAll(".ret3").forEach(ret => {
     ret.classList.remove("pull_up");
@@ -111,4 +113,77 @@ function hideLightbox() {
       ret.style.display = "none";
     }, 400);
   });
+}
+
+
+
+/*let imgCarouselArray = ["billeder/splash.jpg", "billeder/splash2.jpg", "billeder/splash3.jpg", "billeder/splash4.jpg", "billeder/splash5.jpg"]
+let sliceFrom = 4;
+let sliceTo = 5;
+let slicedUrl = imgCarouselArray.slice(sliceFrom, sliceTo);
+imgCarousel();
+
+
+function imgCarousel() {
+
+  if (sliceFrom < 4) {
+    sliceFrom++;
+    sliceTo++;
+  } else {
+    sliceFrom = 0;
+    sliceTo = 1;
+  }
+  slicedUrl = imgCarouselArray.slice(sliceFrom, sliceTo);
+
+  document.querySelector("#splash").style.backgroundImage = `url(${slicedUrl})`;
+
+  setTimeout(imgCarousel, 2000);
+}*/
+
+let splash = document.querySelector("#splash");
+let splash2 = document.querySelector("#splash2");
+let splash3 = document.querySelector("#splash3");
+let splash4 = document.querySelector("#splash4");
+let splash5 = document.querySelector("#splash5");
+
+imgCarousel();
+
+function imgCarousel() {
+  setTimeout(function () {
+    splash.classList.remove("splash_left");
+    splash.classList.add("splash_right");
+    splash2.classList.remove("splash_right");
+    splash2.classList.add("splash_left");
+    setTimeout(function () {
+      splash2.classList.remove("splash_left");
+      splash2.classList.add("splash_right");
+      splash3.classList.remove("splash_right");
+      splash3.classList.add("splash_left");
+      setTimeout(function () {
+        splash3.classList.remove("splash_left");
+        splash3.classList.add("splash_right");
+        splash4.classList.remove("splash_right");
+        splash4.classList.add("splash_left");
+        setTimeout(function () {
+          splash4.classList.remove("splash_left");
+          splash4.classList.add("splash_right");
+          splash5.classList.remove("splash_right");
+          splash5.classList.add("splash_left");
+          setTimeout(function () {
+            splash5.classList.remove("splash_left");
+            splash5.classList.add("splash_right");
+            splash.classList.remove("splash_right");
+            splash.classList.add("splash_left");
+
+            imgCarousel();
+          }, 3000);
+        }, 3000);
+      }, 3000);
+    }, 3000);
+  }, 3000);
+
+
+
+
+
 }
